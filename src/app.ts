85d65ba2b432d552cf8c5ebcad4fca375c67f288
vini-garcia <devvinigarcia@gmail.com>
2024-05-19 16:29:24 -0300

    build: installed cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,17 @@ import "express-async-errors";
 import express, { Application, json } from "express";
 import handleError from "./middlewares/handleError.middleware";
 import { productRouter } from "./routers/product.router";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: [/http:\/\/(localhost|127.0.0.1)(:\d+){0,1}$/],
+    maxAge: 3600,
+  })
+);
+
 app.use(json());
 
 app.use("/products", productRouter);
